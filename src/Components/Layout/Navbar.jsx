6706827d77d94d4c00816c82c898d918113c8361
Kartik-369 <kartikbpadia0707@gmail.com>
2025-12-14 16:01:09 +0530

    Improved Navbar with responsiveness

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -1,17 +1,70 @@
+import { useState } from "react";
+
 function Navbar(){
+  const [isOpen, setOpen] = useState(false);
   return (<>
-    <nav className="justify-around p-4 items-center font-roslindale flex shadow-xs max-w-screen outline-[0.5px] outline-zinc-300 ">
-        <span className="text-4xl tracking-wider   font-bold">Neuro-Business<p className="text-[15px] font-canela font-light tracking-wider ">AI Driven SaaS for Business Insights</p></span>
-        <div className="flex">
-          <div className="space-x-4 text-lg font-sans tracking-wide">
-            <a className="rounded-md px-3 py-2">Platform</a>
-            <a className="rounded-md px-3 py-2">Pricing</a>
-            <a className="rounded-md px-3 py-2">How it Works </a>
-            <a className="rounded-md px-3 py-2">Resources</a>
-          </div>
+    <nav className="relative justify-between p-4 items-center font-roslindale flex shadow-2xs max-w-screen ">
+      <span className="text-4xl tracking-tight max-[1100px]:text-4xl font-bold shrink-0 z-10">NeuroBusiness<p className="text-[14px] font-sans font-semibold tracking-tighter hidden min-[1100px]:flex">AI Driven SaaS for Business Insights</p></span>
+        <div className="hidden absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 items-center min-[1100px]:flex space-x-2 lg:space-x-6 text-lg font-sans tracking-wide">
+          <a className=" px-3 py-2 border-b-0 hover:border-b  duration-75 hover:-translate-y-1 transition-all ease-in">Platform</a>
+          <a className=" px-3 py-2 border-b-0 hover:border-b  duration-75 hover:-translate-y-1 transition-all ease-in">Pricing</a>
+          <a className=" px-3 py-2 border-b-0 hover:border-b  duration-75 hover:-translate-y-1 transition-all ease-in">How it Works </a>
+          <a className=" px-3 py-2 border-b-0 hover:border-b  duration-75 hover:-translate-y-1 transition-all ease-in">Resources</a>
         </div>
-      
+          
+          <div className="flex items-center gap-3 z-20 ">
+            <div className="hidden min-[1100px]:flex tracking-wider font-bold items-center text-lg gap-3">
+              <button className="active:bg-black w-auto px-6   active:text-white text-center border rounded-4xl whitespace-nowrap  p-2  hover:bg-black  hover:text-white  transition">Signup</button>
+              <button className="active:bg-black w-auto px-6   active:text-white text-center  rounded-4xl text-black whitespace-nowrap border p-2  hover:bg-black hover:text-white transition">Book a Demo</button>
+            </div>
+          </div>
+          
+        
+        
+    <button className="min-[1100px]:hidden transition-colors duration-700" onClick={()=>setOpen(!isOpen)}>=</button>
+    
+      <div 
+        className={` min-[1100px]:hidden fixed inset-0 bg-black/10 transition-opacity duration-500 ease-in-out ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`} 
+          onClick={() => setOpen(false)}
+          ></div>
+    
+    
+      <div className={`min-[1100px]:hidden absolute top-full right-0 bg-white h-[calc(100dvh-100%)] overflow-y-auto min-w-80 transform transition-transform ease-in-out duration-400  ${isOpen?'translate-x-0':'translate-x-full'} flex flex-col`}>
+        <div className="flex justify-end pr-6 pt-6 ">
+        <button className="min-[1100px]:hidden transition-colors duration-700" onClick={()=>setOpen(!isOpen)}>X</button>
+        </div>
+        <ul className=" flex flex-col space-y-3 p-6 text-[16.2px]  font-canela tracking-wide ">
+          <li className="border-b border-stone-300">
+            <a className="block px-3 py-2 ">Platform</a>
+          </li>
+          
+          <li className="border-b border-stone-300">
+          <a className="block px-3 py-2">Pricing</a>
+          </li>
+          
+          <li className="border-b border-stone-300">
+            <a className="block px-3 py-2">How it Works </a>
+          </li>
+          
+          <li className="border-b border-stone-300">
+            <a className="block px-3 py-2">Resources</a>
+          </li>
+        </ul>
+        
+        <div className="mt-auto p-6 space-y-6 text-lg tracking-wider font-bold">
+          <button className="active:bg-black w-full text-center bg-gray-900 rounded-4xl text-white p-2  hover:bg-black transition">Signup</button>
+          <button className="active:bg-black active:text-white w-full text-center  rounded-4xl text-black border p-2  hover:bg-black hover:text-white transition">Book a Demo</button>
+          
+        </div>
+        
+    
+        
+    </div>
+
+    
+    
     </nav>
+  
   </>);
 }
 

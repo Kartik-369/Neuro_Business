9c0376ebc5bda33d1167028ea1da93bbe3444ab9
Kartik-369 <kartikbpadia0707@gmail.com>
2025-12-15 18:42:36 +0530

    Added Sticky Scroll Cards and improved responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Home from "./Pages/Home";
+import Scroll from "./Components/Scroll";
 import Layout from "./Components/Layout/Layout";
 function App(){
   return (<>
-    <Layout>
-      <Home/>
-    </Layout>
+    <Scroll>
+      <Layout>
+        <Home/>
+      </Layout>
+    </Scroll>
   </>);
 }
 export default App;

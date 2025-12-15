73f111df8aef21c3e25c32358a67965cf6fd911b
Kartik-369 <kartikbpadia0707@gmail.com>
2025-12-15 18:42:36 +0530

    Added Sticky Scroll Cards and improved responsiveness

---
+++ src/Components/Scroll.jsx
@@ -0,0 +1,37 @@
+import { useLayoutEffect, useRef } from 'react'
+import Lenis from '@studio-freight/lenis'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+function Scroll({ children }) {
+  const lenisRef = useRef()
+
+  useLayoutEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.5,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    })
+
+    lenisRef.current = lenis
+
+    lenis.on('scroll', ScrollTrigger.update)
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000)
+    })
+
+    gsap.ticker.lagSmoothing(0)
+
+    return () => {
+      gsap.ticker.remove(lenis.raf)
+      lenis.destroy()
+    }
+  }, [])
+
+  return <div className="w-full min-h-screen">{children}</div>
+}
+
+export default Scroll;

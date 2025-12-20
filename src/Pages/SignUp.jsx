307147be2a007b53e09dffdf926ac6ec92210d2b
Kartik-369 <kartikbpadia0707@gmail.com>
2025-12-20 23:15:54 +0530

    Basic routing setup

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -2,7 +2,7 @@
 function SignUp(){
   return (<>
     <div className="flex justify-center items-center">
-      <div className="p-5 bg-amber-50">
+      <div className="p-5 mt-24 m-3 rounded-4xl h-100 bg-orange-100">
         
       </div>
     </div>

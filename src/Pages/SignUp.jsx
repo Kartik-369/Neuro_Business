a0e58c2b00e2334d3fb193a0e1416d4acf67251b
Kartik-369 <kartikbpadia0707@gmail.com>
2025-12-20 22:59:35 +0530

    added router

---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,12 @@
+
+function SignUp(){
+  return (<>
+    <div className="flex justify-center items-center">
+      <div className="p-5 bg-amber-50">
+        
+      </div>
+    </div>
+  </>);
+}
+
+export default SignUp;

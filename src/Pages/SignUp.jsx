6f831fb9b7b74cb02ad603ab4c1aa97753735fde
Kartik-369 <kartikbpadia0707@gmail.com>
2025-12-21 11:23:37 +0530

    Small fixes

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -12,8 +12,8 @@ function SignUp(){
             <div className="flex flex-row justify-between items-center mt-3">
               <h1 className=" text-2xl font-semibold text-gray-800 capitalize sm:text-3xl">{Login?'Sign In':'Sign Up'}</h1>
                 <div className="flex justify-center items-center text-2xl">
-                  <Link to='/'><button className="bg-white text-center w-24 rounded-2xl h-14 relative text-black text-xl font-semibold group" type="button">Go Back
-                  <div class="bg-amber-100 rounded-xl h-12 w-1/2 flex items-center justify-center absolute -left-12 top-[4px] group-hover:w-[144px] group-active:w-[144px] z-10 duration-300">
+                  <Link to='/'><button className="bg-white text-center w-24 rounded-2xl h-14 relative text-black text-[15px] font-semibold group" type="button">Go Back
+                  <div class="bg-amber-100 rounded-xl h-[39px] w-1/3 flex items-center justify-center absolute -left-6 top-[9px] group-hover:w-[120px] group-active:w-[120px] z-10 duration-300">
                     <svg
                           xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 1024 1024"
@@ -63,7 +63,7 @@ function SignUp(){
                   <Link to='/forgot' className="w-fit"><p className="mt-2 text-sm underline inline-block text-center">{Login?'forgot password?':''}</p></Link>
                   <p className="mt-1 text-center text-gray-600 ">{Login?'or':'Or continue'}</p>
   
-                  <a href="" className="flex items-center justify-center px-6 py-3 mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg  hover:bg-amber-100">
+                  <a href="" className=" hover:text-black active:text-black flex items-center justify-center px-6 py-3 mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg  hover:bg-amber-100">
                       <svg className="w-6 h-6 mx-2" viewBox="0 0 40 40">
                           <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
                           <path d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z" fill="#FF3D00" />
@@ -71,7 +71,7 @@ function SignUp(){
                           <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#1976D2" />
                       </svg>
   
-                      <span className="mx-2 hover:text-black active:text-black">{Login?'Sign In':'Sign Up'} with Google</span>
+                      <span className="mx-2">{Login?'Sign In':'Sign Up'} with Google</span>
                   </a>
   
                   <div className="mt-6 text-center ">

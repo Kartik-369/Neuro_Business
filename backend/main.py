23020073f847f6cb6e70332a81cfd6e9fe947342
Kartik-369 <kartikbpadia0707@gmail.com>
2025-12-25 18:54:04 +0530

    Fixed backend dependencies

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,6 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
-# from database import check_db_connection
-from routes import user  # <--- NEW LINE 1
+app=FastAPI()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await check_db_connection()
-    yield
-
-app = FastAPI(lifespan=lifespan)
-
-# This connects the user routes to the main app
-app.include_router(user.router) # <--- NEW LINE 2
-
-@app.get("/")
-async def root():
-    return {"message": "NeuroBusiness Backend is Running"}
+@app.get('/')
+def home():
+	return {'hello'}

812e045ee1b0dc3a845a1b682a32478cf21320cf
Kartik-369 <kartikbpadia0707@gmail.com>
2025-12-27 19:24:12 +0530

    user.py: basic registration and Mongo communication (docker)

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from database import db_connec
+from routes import user
 @asynccontextmanager
 async def life(app:FastAPI):
 	await db_connec()
 	yield
 
 app=FastAPI(lifespan=life)
+
+app.include_router(user.router)
 
 @app.get('/')
 def home():

ee85cc9ab7c824d123f724c6a205eec69673b884
Kartik-369 <kartikbpadia0707@gmail.com>
2026-01-04 16:24:21 +0530

    data.py: added csv and xlsx file read

--- backend/routes/data.py
+++ backend/routes/data.py
@@ -1,5 +1,8 @@
 from ast import Await, Return
-from fastapi import APIRouter,UploadFile,File
+from fastapi import APIRouter,UploadFile,File,HTTPException
+import pandas as pd
+import os
+
 router = APIRouter()
 
 @router.post('/upload')
@@ -11,3 +14,33 @@ async def upload_file(file:UploadFile=File(...)):
         'size':len(content),
         'message':'File received successfully!'
     }
+
+
+@router.get('/columns')
+async def get_cols():
+    folder='uploaded_files'
+
+    if not os.path.exists(folder):
+        return {'error':'no file upload'}
+
+    file = os.listdir(folder)
+    if not files:
+        return {'error':'no files found'}
+
+    latest_file=max([os.path.join(folder,f) for f in files], key=os.path.getctime)
+
+    try:
+        if latest_file.endswith('.csv'):
+            df=pd.read_csv('latest_file')
+        elif latest_file.endswith('.xlsx'):
+            df=pd.read_excel('latest_file')
+        else:
+            return {'error':'file not supported'}
+
+        return{
+            'filename':os.path.basename(latest_file),
+            'columns':list(df.columns),
+            'preview':df.head(5).to_dict(orient='records')
+        }
+    except Exception as e:
+        return {'error':str(e)}

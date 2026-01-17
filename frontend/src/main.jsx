4f85d8b5621cd4b373ad785778d61bf62dbe0bfd
Kartik-369 <kartikbpadia0707@gmail.com>
2026-01-17 16:12:41 +0530

    Improved Navbar: Added MUI components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import {StyledEngineProvider} from '@mui/material/styles'
 import './index.css'
 import App from './App.jsx'
+// import { StyledEngineProvider } from '@mui/material'
 
 createRoot(document.getElementById('root')).render(
   <StyledEngineProvider>
   <StrictMode>
     <App />
   </StrictMode>
+<<<<<<< HEAD
 </StyledEngineProvider>
+=======
+</StyledEngineProvider>,
+>>>>>>> 40ed743 (Improved Navbar: Added MUI components)
 )

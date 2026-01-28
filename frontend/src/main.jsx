84778de67617c4399b0b98fd7fdb72ccde590629
Kartik Padia <kartikbpadia0707@gmail.com>
2026-01-28 23:58:36 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import {StyledEngineProvider} from '@mui/material/styles'
 import './index.css'
 import App from './App.jsx'
-// import { StyledEngineProvider } from '@mui/material'
 
 createRoot(document.getElementById('root')).render(
   <StyledEngineProvider>

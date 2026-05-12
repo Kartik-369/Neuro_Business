38e54e585dcc3bbf64ac21154c1aa66231f0b166
kartik-369 <kartikbpadia0707@gmail.com>
2026-05-12 13:00:11 +0530

    Route for charts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import SignUp from "./Pages/SignUp";
 import Forgot from "./Pages/Forgot";
 import Upload from "./Pages/Upload";
+import Dashboard from "./Pages/Dashboard";
+import Chart_test from "./Pages/Chart_test";
 function App(){
   return (<>
     
@@ -15,7 +17,9 @@ function App(){
             <Route path='/' element={<Home/>} />
             <Route path='/signup' element={<SignUp/>} />
             <Route path='/forgot' element={<Forgot/>}/>
-            <Route path='/upload' element={<Upload/>}/>
+            <Route path='/upload' element={<Upload />} />
+            <Route path="/predict" element={<Dashboard />}/>
+            <Route path="/chart" element={<Chart_test/>}/>
           </Routes>
           
         </Layout>

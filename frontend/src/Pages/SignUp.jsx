be6b24b2ca5f00cb3b87d4b6f7bbf24736008c13
kartik-369 <kartikbpadia0707@gmail.com>
2026-05-13 12:04:33 +0530

    Updated for google auth and forgot password

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -1,49 +1,38 @@
-import { useNavigate,Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react";
+import { useGoogleLogin } from "@react-oauth/google";
 
 function SignUp(){
   const [Login, isLogin] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
   const navigate = useNavigate();
-  
-  const manageAuth = async () => {
+  const manageAuth=async()=>{
       if (!email || !password) {
         alert("Please fill in the details");
         return;
       }
-  
       const end = Login ? '/login' : '/register';
       const url = `http://127.0.0.1:8000${end}`;
-  
       try {
         let response;
-  
         if (Login) {
           const formData = new URLSearchParams();
           formData.append('username', email);
           formData.append('password', password);
-  
           response = await fetch(url, {
             method: 'POST',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             body: formData.toString(),
           });
-  
         } else {
           response = await fetch(url, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              email: email,
-              password: password,
-            }),
+            body: JSON.stringify({ email: email, password: password }),
           });
         }
-  
         const data = await response.json();
-  
         if (response.ok) {
           if (Login) {
             localStorage.setItem('token', data.access_token);
@@ -62,12 +51,36 @@ function SignUp(){
         alert("Network Error: " + error);
       }
     }
-  
-  
+  const loginWithGoogle = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      console.log("Google Token", tokenResponse.access_token);
+      try {
+        const response = await fetch("http://127.0.0.1:8000/auth/google", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ token: tokenResponse.access_token }), 
+        });
+        const data = await response.json();
+        
+        if(response.ok){
+            localStorage.setItem('token', data.access_token);
+            alert('Login successful!');
+            window.dispatchEvent(new Event('authChange'));
+            navigate('/upload');
+        } else {
+            alert('Auth Failed');
+        }
+      } catch (error) {
+        console.error("rejected token:", error);
+      }
+    },
+    onError: () => console.log('Google Login Failed'),
+  });
+
+
   return (<>
-  <section className="bg-white ">
     
-    
+    <section className="bg-white ">
       <div className="container  flex items-center justify-center min-h-screen px-6 mx-auto">
         
           <form className="w-full shadow-xs shadow-gray-600 border border-stone-200 bg-amber-50/30 p-9 rounded-4xl max-w-md">  
@@ -122,19 +135,18 @@ function SignUp(){
                   <button onClick={manageAuth} type="button" className="w-full block text-center px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-600 rounded-lg hover:bg-emerald-600 focus:outline-none focus:ring focus:ring-green-300 focus:ring-opacity-50">
                       {Login?'Sign In':'Sign Up'}
                   </button>
-                  <button to='/forgot' className="w-fit"><p className="mt-2 text-sm underline inline-block text-center">{Login?'forgot password?':''}</p></button>
+                  {Login && <Link to='/forgot'><button className="w-fit"><p className="mt-2 text-sm underline inline-block text-center">forgot password?</p></button></Link>}
                   <p className="mt-1 text-center text-gray-600 ">{Login?'or':'Or continue'}</p>
   
-                  <a href="" className=" hover:text-black active:text-black flex items-center justify-center px-6 py-3 mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg  hover:bg-amber-100">
-                      <svg className="w-6 h-6 mx-2" viewBox="0 0 40 40">
-                          <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
-                          <path d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z" fill="#FF3D00" />
-                          <path d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z" fill="#4CAF50" />
-                          <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#1976D2" />
-                      </svg>
-  
-                      <button to='' className="mx-2 block">{Login?'Sign In':'Sign Up'} with Google</button>
-                  </a>
+                  <button type="button" onClick={() => loginWithGoogle()} className="w-full hover:text-black active:text-black flex items-center justify-center px-6 py-3 mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-amber-100">
+                    <svg className="w-6 h-6 mx-2" viewBox="0 0 40 40">
+                      <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
+                      <path d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z" fill="#FF3D00" />
+                      <path d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z" fill="#4CAF50" />
+                      <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#1976D2" />
+                    </svg>
+                    <span className="mx-2 block">{Login ? 'Sign In' : 'Sign Up'} with Google</span>
+                  </button>
   
                   <div className="mt-6 text-center ">
               <a className="text-sm text-gray-600 hover:underline" onClick={() => { isLogin(!Login)}}>

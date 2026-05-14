fdef52a9c9e150dba9d7baa1e353fb1233963bdc
kartik-369 <kartikbpadia0707@gmail.com>
2026-05-14 22:09:00 +0530

    added google auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {StyledEngineProvider} from '@mui/material/styles'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { StyledEngineProvider } from "@mui/material/styles";
+import "./index.css";
+import App from "./App.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 // import { StyledEngineProvider } from '@mui/material'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StyledEngineProvider>
-  <StrictMode>
-    <App />
-  </StrictMode>
-</StyledEngineProvider>
-)
+    <StrictMode>
+      <GoogleOAuthProvider clientId="512413607874-ibj6ud3uvnh5074h0g2q59uak87nu53p.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
+    </StrictMode>
+  </StyledEngineProvider>,
+);

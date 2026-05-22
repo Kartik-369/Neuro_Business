e13a9a22096ebd5c138ff35b579696b673f9a038
kartik-369 <kartikbpadia0707@gmail.com>
2026-05-22 21:25:06 +0530

    added vercel url to CORS

--- backend/main.py
+++ backend/main.py
@@ -14,11 +14,12 @@ app=FastAPI(lifespan=life)
 
 origins=[
     "http://localhost:5173/",
-    "http://127.0.0.1:8000/"
+    "http://127.0.0.1:8000/",
+    "https://neuro-business.vercel.app"
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
